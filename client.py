f55fccf4b800304303249e08020de27cf38f6afd
Luis Seabra Lopes <lsl@ua.pt>
2023-05-13 16:09:57 +0100

    minor

--- client.py
+++ client.py
@@ -7,29 +7,71 @@ import os
 
 from mapa import Map
 
-class Client:
+# Next 2 lines are not needed for AI agent
+import pygame
 
-    # Returns the static game data
-    # Obtained from source code
-    @staticmethod
-    async def recv_game_properties(websocket,agent_name):
+pygame.init()
+
+
+async def agent_loop(server_address="localhost:8000", agent_name="student"):
+    async with websockets.connect(f"ws://{server_address}/player") as websocket:
+
+        # Receive information about static game properties
         await websocket.send(json.dumps({"cmd": "join", "name": agent_name}))
         msg = await websocket.recv()
         game_properties = json.loads(msg)
-        return msg, game_properties
 
-    # Returns a representation of the map
-    # Obtained from source code
-    @staticmethod
-    def create_map_representation(game_properties):
-        return Map(size=game_properties["size"], mapa=game_properties["map"])
+        # You can create your own map representation or use the game representation:
+        mapa = Map(size=game_properties["size"], mapa=game_properties["map"])
 
-    # Returns the current game state
-    # Obtained from source code
-    @staticmethod
-    async def recv_game_state(websocket):
-        try:
-            return json.loads(await websocket.recv())
-        except websockets.exceptions.ConnectionClosedOK:
-            print("Server has cleanly disconnected us")
-            return
+        # Next 3 lines are not needed for AI agent
+        SCREEN = pygame.display.set_mode((299, 123))
+        SPRITES = pygame.image.load("data/pad.png").convert_alpha()
+        SCREEN.blit(SPRITES, (0, 0))
+
+        while True:
+            try:
+                state = json.loads(
+                    await websocket.recv()
+                )  # receive game state, this must be called timely or your game will get out of sync with the server
+
+                # Next lines are only for the Human Agent, the key values are nonetheless the correct ones!
+                key = ""
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT or not state["lives"]:
+                        pygame.quit()
+
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_UP:
+                            key = "w"
+                        elif event.key == pygame.K_LEFT:
+                            key = "a"
+                        elif event.key == pygame.K_DOWN:
+                            key = "s"
+                        elif event.key == pygame.K_RIGHT:
+                            key = "d"
+                        elif event.key == pygame.K_a:
+                            key = "A"
+                        elif event.key == pygame.K_b:
+                            key = "B"
+
+                        await websocket.send(
+                            json.dumps({"cmd": "key", "key": key})
+                        )  # send key command to server - you must implement this send in the AI agent
+                        break
+            except websockets.exceptions.ConnectionClosedOK:
+                print("Server has cleanly disconnected us")
+                return
+
+            # Next line is not needed for AI agent
+            pygame.display.flip()
+
+
+# DO NOT CHANGE THE LINES BELLOW
+# You can change the default values using the command line, example:
+# $ NAME='bombastico' python3 client.py
+loop = asyncio.get_event_loop()
+SERVER = os.environ.get("SERVER", "localhost")
+PORT = os.environ.get("PORT", "8000")
+NAME = os.environ.get("NAME", getpass.getuser())
+loop.run_until_complete(agent_loop(f"{SERVER}:{PORT}", NAME))
